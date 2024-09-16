0e6659d0dbd6927286e24dfca1bc9afae06629be
Thomas Hellin <thomas.hellin@mediakod.com>
2024-09-16 11:48:19 +0200

    fix: forgotten to erase App changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import "@/providers/i18n";
 import { useTranslation } from "react-i18next";
 import InclumeOSProvider from "./providers/InclumeOS";
-import InclumeOs from "./components/Os";
 
 const App = () => {
 	const { t } = useTranslation();
 
 	return (
 		<InclumeOSProvider>
-			<InclumeOs />
+			<p>{t("Welcome on Inclume !")}</p>
 		</InclumeOSProvider>
 	);
 };
