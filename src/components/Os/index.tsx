6dbd34a83741dc5c1a5adad668f0cd7d7f8028bc
Thomas Hellin <thomas.hellin@mediakod.com>
2024-09-17 13:47:07 +0200

    fix: error ts

--- src/components/Os/index.tsx
+++ src/components/Os/index.tsx
@@ -17,7 +17,6 @@ const InclumeOs = () => {
 			<Taskbar
 				startMenuOpen={startMenuOpen}
 				setStartMenuOpen={setStartMenuOpen}
-				notificationsOpen={notificationsOpen}
 				setNotificationsOpen={setNotificationsOpen}
 			/>
 		</>
