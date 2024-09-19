d556b140af5e00df886204fa60b5a1dbcd613aa0
Logan Blangenois <logan.blangenois@mediakod.com>
2024-09-19 14:51:22 +0200

    feat: improve PWA

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,31 @@ import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), svgr()],
+	plugins: [
+		react(),
+		svgr(),
+		VitePWA({
+			manifest: {
+				name: "Inclume",
+				short_name: "Inclume",
+				start_url: "/",
+				background_color: "#ffffff",
+				theme_color: "#000000",
+				display: "fullscreen",
+				icons: [
+					{
+						src: "/favicon.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+				],
+			},
+		}),
+	],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"), // Alias pour le dossier 'src',
