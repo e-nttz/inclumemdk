8ee4a3d887b62608dd70344179669f9910dedc83
Thomas Hellin <thomas.hellin@mediakod.com>
2024-09-19 14:43:02 +0200

    feat: add ContextualBar component to window

---
+++ src/components/Os/Window/ContextualBar/BarItem.tsx
@@ -0,0 +1,60 @@
+import { classNames } from "@/helpers/sanitize";
+import { useClickOutside } from "@/hooks/useClickOutside";
+import { useRef, useState } from "react";
+
+interface BarItemProps {
+	name: string;
+	openedItem: string;
+	setOpenedItem: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const BarItem = ({ name, openedItem, setOpenedItem }: BarItemProps) => {
+	const handleOpening = () => {
+		if (openedItem === name) {
+			setOpenedItem("");
+		} else {
+			setOpenedItem(name);
+		}
+	};
+
+	const isOpened = openedItem === name;
+
+	const submenuRef = useRef<HTMLUListElement>(null);
+
+	useClickOutside(submenuRef, (e: MouseEvent) => {
+		// Check if e.target has the id of start-menu, or if it's a child of start-menu
+		if ((e.target as HTMLElement).closest(".baritem-submenu-list")) {
+			return;
+		}
+
+		setOpenedItem("");
+	});
+
+	return (
+		<li className="relative baritem-submenu-list">
+			<button
+				className="px-2 py-1 text-sm transition hover:bg-black hover:bg-opacity-10 rounded-[3px]"
+				onClick={handleOpening}
+			>
+				{name}
+			</button>
+			<ul
+				className={classNames(
+					"absolute left-0 bg-white bg-opacity-90 backdrop-blur-xl top-[calc(100%+6px)] w-52 rounded-md shadow-lg space-y-2 z-10 p-2 dark:bg-black transition",
+					isOpened
+						? "translate-y-0 opacity-100 visible"
+						: "-translate-y-2 opacity-0 invisible"
+				)}
+				ref={submenuRef}
+			>
+				<li>
+					<button className="w-full px-2 text-left hover:bg-black hover:bg-opacity-5 rounded-[4px] py-2 transition text-sm">
+						Fermer l'application
+					</button>
+				</li>
+			</ul>
+		</li>
+	);
+};
+
+export default BarItem;
--- src/components/Os/Window/Topbar.tsx
+++ src/components/Os/Window/Topbar.tsx
@@ -1,17 +1,9 @@
 import { useOS } from "@/providers/InclumeOS";
+import ContextualBar from "./ContextualBar";
+import ControlButtons from "./ControlButtons";
 
 const Topbar = () => {
-	const { currentApp, setCurrentApp, setOpenedApps, openedApps } = useOS();
-
-	const handleReduceButton = () => {
-		setCurrentApp("");
-	};
-
-	const handleCloseButton = () => {
-		const openedAppsCopy = [...openedApps];
-		setOpenedApps(openedAppsCopy.filter((app) => app.name !== currentApp));
-		setCurrentApp("");
-	};
+	const { currentApp, openedApps } = useOS();
 
 	const currentAppIcon = openedApps.find(
 		(app) => app.name === currentApp
@@ -28,95 +20,9 @@ const Topbar = () => {
 						<>{currentAppIcon}</>
 					</figure>
 				</div>
-				<ul className="flex items-center h-full gap-2">
-					<li className="flex items-center justify-center h-[80%]">
-						<button
-							className="h-full px-2 transition rounded-md hover:bg-black hover:bg-opacity-20 focus-visible:bg-black focus-visible:bg-opacity-20 focus-visible:outline-none dark:hover:bg-white dark:hover:bg-opacity-10"
-							onClick={handleReduceButton}
-						>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								width="14"
-								height="2"
-								fill="none"
-								viewBox="0 0 14 2"
-								aria-hidden="true"
-							>
-								<path
-									fill="currentColor"
-									d="M.528 1a.5.5 0 01.5-.5h12a.5.5 0 110 1h-12a.5.5 0 01-.5-.5z"
-								></path>
-							</svg>
-							<span className="sr-only">Réduire l'application</span>
-						</button>
-					</li>
-					<li className="flex items-center justify-center h-[80%]">
-						<button className="h-full px-2 transition rounded-md hover:bg-black hover:bg-opacity-20 focus-visible:bg-black focus-visible:bg-opacity-20 focus-visible:outline-none dark:hover:bg-white dark:hover:bg-opacity-10">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 21 19"
-								aria-hidden="true"
-								className="w-[14px] h-auto"
-							>
-								<path
-									stroke="currentColor"
-									strokeWidth="1.824"
-									d="M1.713 0.112L1.713 18.351"
-								></path>
-								<path
-									stroke="currentColor"
-									strokeWidth="1.824"
-									d="M19.207 0.112L19.207 18.351"
-								></path>
-								<path
-									stroke="currentColor"
-									strokeWidth="1.824"
-									d="M20.239 1.024L0.801 1.024"
-								></path>
-								<path
-									stroke="currentColor"
-									strokeWidth="1.824"
-									d="M20.239 17.439L0.801 17.439"
-								></path>
-							</svg>
-							<span className="sr-only">Bouton factice</span>
-						</button>
-					</li>
-					<li className="flex items-center justify-center h-[80%]">
-						<button
-							className="h-full px-2 transition rounded-md hover:bg-black hover:bg-opacity-20 focus-visible:bg-black focus-visible:bg-opacity-20 focus-visible:outline-none dark:hover:bg-white dark:hover:bg-opacity-10"
-							onClick={handleCloseButton}
-						>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								width="14"
-								height="14"
-								fill="none"
-								viewBox="0 0 14 14"
-								aria-hidden="true"
-							>
-								<path
-									stroke="currentColor"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									d="M.528 1l6 6m6 6l-6-6m0 0l-6 6 12-12"
-								></path>
-							</svg>
-							<span className="sr-only">Fermer l'application</span>
-						</button>
-					</li>
-				</ul>
+				<ControlButtons />
 			</div>
-			<div className="flex items-center h-10 px-4 bg-slate-100 dark:bg-slate-700 dark:backdrop-blur-3xl">
-				<ul>
-					<li>
-						<button className="px-2 py-1 text-sm transition hover:bg-black hover:bg-opacity-10 rounded-[3px]">
-							Fichier
-						</button>
-					</li>
-				</ul>
-			</div>
+			<ContextualBar />
 		</header>
 	);
 };
