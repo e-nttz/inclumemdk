2de7e7fe5a6ac027b38add17a4aef0f73421d2aa
Thomas Hellin <thomas.hellin@mediakod.com>
2024-09-23 14:40:19 +0200

    feat(browser): add tabs management

--- src/components/Os/Window/Topbar.tsx
+++ src/components/Os/Window/Topbar.tsx
@@ -1,6 +1,7 @@
 import { useOS } from "@/providers/InclumeOS";
 import ContextualBar from "./ContextualBar";
 import ControlButtons from "./ControlButtons";
+import { classNames } from "@/helpers/sanitize";
 
 const Topbar = ({ contextMenus }) => {
 	const { currentApp, openedApps } = useOS();
@@ -10,7 +11,12 @@ const Topbar = ({ contextMenus }) => {
 	)?.icon;
 
 	return (
-		<header className="top-0 left-0 z-10 w-full h-20 ">
+		<header
+			className={classNames(
+				"top-0 left-0 z-10 w-full",
+				contextMenus ? "h-20" : "h-10"
+			)}
+		>
 			<div className="flex items-center justify-between h-10 px-4 border-b dark:border-gray-600/-20 border-gray-300/20 bg-white/75 backdrop-blur-lg backdrop-filter dark:bg-gray-800/75 dark:backdrop-blur-lg dark:backdrop-filter">
 				<div className="flex flex-row-reverse items-center">
 					<h2 className="px-2 text-sm font-medium text-black pointer-events-none text-opacity-80 dark:text-white">
@@ -23,7 +29,7 @@ const Topbar = ({ contextMenus }) => {
 				<ControlButtons />
 			</div>
 
-			<ContextualBar>{contextMenus}</ContextualBar>
+			{contextMenus && <ContextualBar>{contextMenus}</ContextualBar>}
 		</header>
 	);
 };
