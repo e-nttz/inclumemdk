52848deeda2fe7b19c587c792b7e6abeb70e31ef
Logan Blangenois <logan.blangenois@mediakod.com>
2024-09-23 16:18:30 +0200

    fix(main): reset StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,11 @@ import App from "./App.tsx";
 
 import "@/assets/styles/main.scss";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<App />
+	</StrictMode>
+);
 
 // (function (global) {
 // 	if (typeof global === "undefined") {
