bd9c5e7b786114afe1d46211d9f3bd113c369b7d
Thomas Hellin <thomas.hellin@mediakod.com>
2024-10-24 14:28:49 +0200

    feat: follow of the ui in browser

--- src/components/Apps/Browser.exe/Websites/Welcome/index.tsx
+++ src/components/Apps/Browser.exe/Websites/Welcome/index.tsx
@@ -5,15 +5,17 @@ import ButtonWebsite from "./ButtonWebsite";
 const Welcome: Website = () => {
 	return (
 		<div className="absolute inset-0 px-8 py-8">
-			<div className="relative z-10">
-				<h1 className="mb-8 text-3xl text-white">Vos sites favoris</h1>
-				<ul className="grid grid-cols-12 gap-x-4 gap-y-8">
-					<ButtonWebsite website={SearchEngine} />
-				</ul>
+			<div className="relative z-10 flex items-center w-full h-full max-w-6xl mx-auto">
+				<div className="w-full">
+					<h1 className="text-2xl font-bold text-black mb-9">Favoris</h1>
+					<ul className="grid grid-cols-4 gap-x-5 gap-y-6">
+						<ButtonWebsite website={SearchEngine} />
+					</ul>
+				</div>
 			</div>
-			<figure className="absolute inset-0 before:absolute before:inset-0 before:bg-black before:bg-opacity-60">
+			<figure className="absolute inset-0">
 				<img
-					src="/images/welcome-tab-bg.webp"
+					src="/images/welcome-tab-bg.jpeg"
 					alt=""
 					className="object-cover w-full h-full"
 				/>
