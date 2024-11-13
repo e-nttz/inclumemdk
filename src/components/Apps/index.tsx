57edc853ba7ca178078badac658845fedd1b2cd6
Thomas Hellin <thomas.hellin@mediakod.com>
2024-11-13 10:35:48 +0100

    feat: add texteditor app

--- src/components/Apps/index.tsx
+++ src/components/Apps/index.tsx
@@ -3,6 +3,7 @@ import Explorer from "./Explorer.exe";
 import FaceTime from "./FaceTime.exe";
 import Message from "./Message.exe";
 import Spreadsheets from "./Spreadsheets.exe";
+import TextEditor from "./TextEditor.exe";
 
 interface Apps {
 	[key: string]: React.FC;
@@ -13,6 +14,7 @@ export const apps: Apps = {
 	browser: Browser,
 	explorer: Explorer,
 	stylesheets: Spreadsheets,
+	textEditor: TextEditor,
 };
 
 const RenderAllApps = () => {
